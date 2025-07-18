5c6740a0925f2538bcc8cdbe78ec297e0c7408ea
Stanley Buenaventura <105530737+SBSonk@users.noreply.github.com>
2025-07-18 19:49:36 +0800

    tite

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,37 @@
 #include <iostream>
 #include <string>
 
+const int screenWidth = 640;
+const int screenHeight = 480;
+
+class Player {
+    public:
+        Vector2 position;
+        int radius;
+        int moveSpeed;
+
+        void Draw() {
+            DrawCircle(position.x, position.y, radius, PURPLE);
+        }
+
+        void Update(Vector2 input) {
+            position.x += input.x * moveSpeed;
+            position.y += input.y * moveSpeed;
+
+            if (position.x > screenWidth - radius) CloseWindow();
+            if (position.x < radius) CloseWindow();
+            if (position.y > screenHeight - radius) CloseWindow();
+            if (position.y < radius) CloseWindow();
+        }
+};
+
 int main()
 {
-    const int screenWidth = 640;
-    const int screenHeight = 480;
-
     InitWindow(screenWidth, screenHeight, "Pong");
 
     SetTargetFPS(60);
 
-    int radius = 25;
-    int startPosX = screenWidth / 2;
-    int startPosY = screenHeight / 2;
-    Vector2 position = {0, 0};
-    position.x = startPosX;
-    position.y = startPosY;
-
-    int moveSpeed = 5;
+    Player player = {{screenWidth / 2, screenHeight / 2}, 25, 5};
 
     while (!WindowShouldClose())
     {
@@ -28,17 +42,10 @@ int main()
         if (IsKeyDown(KEY_D)) input.x = 1;
         if (IsKeyDown(KEY_W)) input.y = -1;
         if (IsKeyDown(KEY_S)) input.y = 1;
-
-        position.x += input.x * moveSpeed;
-        position.y += input.y * moveSpeed;
-
-        // check if collide
-        if (position.x > screenWidth - radius) CloseWindow();
-        if (position.x < radius) CloseWindow();
-        if (position.y > screenHeight - radius) CloseWindow();
-        if (position.y < radius) CloseWindow();
-
+        
         std::string inputStr = "INPUT: (" + std::to_string(input.x) + ", " + std::to_string(input.y) + ")";
+
+        player.Update(input);
 
         // DRAW
         BeginDrawing();
@@ -46,7 +53,7 @@ int main()
 
         DrawText(inputStr.c_str(), 0, 0, 20, BLACK);
 
-        DrawCircle(position.x, position.y, radius, PURPLE);
+        player.Draw();
 
         EndDrawing();
     }
