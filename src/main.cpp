90cedc46b36d4eb46809082c1c488b628192f3f5
Noah <noah.penaranda@ciit.edu.ph>
2025-07-18 17:06:21 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
-#include "Market.h"
-#include "OrderQueue.h"
-#include "Player.h"
-#include "Upgrades.h"
-#include "Utils.h"
 using namespace std;
 
 int main()
 {
-    Market market;
 
-    for (int i = 1; i <= 5; i++)
-    {
-        market.updatePrice();
-        cout << "Market price: " << market.getCurrentPrice() << endl;
-    }
     return 0;
 }
